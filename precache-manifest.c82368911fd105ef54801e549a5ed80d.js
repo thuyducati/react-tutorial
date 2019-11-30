b672ace7f28d5696241d36b05d6fe53ceb151c58
TuongThuy <thuyschannel2301@gmail.com>
2019-12-01 05:29:30 +0700

    Updates

---
+++ precache-manifest.c82368911fd105ef54801e549a5ed80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d5c810c04626127db5f2acd6d11634",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "c1c6d3e92b8975a84f5f",
+    "url": "/react-tutorial/static/css/main.c34cda36.chunk.css"
+  },
+  {
+    "revision": "4afc7388658d3ab9e2e8",
+    "url": "/react-tutorial/static/js/2.b39b814a.chunk.js"
+  },
+  {
+    "revision": "c1c6d3e92b8975a84f5f",
+    "url": "/react-tutorial/static/js/main.3dbeb77b.chunk.js"
+  },
+  {
+    "revision": "23cd54c0b15ae01a9f16",
+    "url": "/react-tutorial/static/js/runtime-main.09266777.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-tutorial/static/media/logo.25bf045c.svg"
+  }
+]);
